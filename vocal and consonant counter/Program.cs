18f4fd5e8b45f75c8cac557044d8edb3a5455a9f
Myseie <123580372+Myseie@users.noreply.github.com>
2024-09-22 14:11:52 +0200

    Add files via upload

---
+++ vocal and consonant counter/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Skriv in en mening så vi kan räkna konsonanter :");
+        string input = Console.ReadLine().ToLower();
+
+        int vowels = 0;
+        int consonants = 0;
+
+        string vowelList = "aeiou";
+
+        foreach (char c in input)
+        {
+            if (vowelList.Contains(c))
+            {
+                vowels++;
+            }
+            else if (char.IsLetter(c))
+            {
+                consonants++;
+            }
+        }
+
+        Console.WriteLine($"Antal vokaler {vowels}");
+        Console.WriteLine($"Antal konsonanter {consonants}");
+    }
+}
